e97ef42bd88520294fc6df26c71c87d6bef4dd0e
ShortCodes19 <jumak4863@gmail.com>
2026-09-10 08:24:22 +0500

    started the backend phase

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/AppDB.js";
-import { errorMiddleware, notFound } from "./middleware/errorMiddleware.js";
-import NoteRoute from "./routes/NoteRoute.js";
-import authRoutes from "./routes/authRoutes.js";
-import cors from "cors";
 
 dotenv.config();
 connectDB();
+
 const app = express();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true,
-  }),
-);
 
 app.get("/", (req, res) => {
-  res.send("App is running");
+  res.send("App is working");
 });
 
-// routes
-app.use("/api/notes", NoteRoute);
-app.use("/api/auth", authRoutes);
-
-// error Middlewares
-app.use(notFound);
-app.use(errorMiddleware);
-
-const PORT = process.env.PORT || 2020;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port : ${PORT}`);
+  console.log(`Server is running on port: ${PORT}`);
 });
